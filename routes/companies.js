6e923f5101d26bc89500de74014946e7c85120d2
Brice Nelson <bnelsonemail@icloud.com>
2025-01-07 23:47:46 -0500

    revisions to company routes.  Added query for company id

--- routes/companies.js
+++ routes/companies.js
@@ -8,7 +8,7 @@ const db = require("../db");
 
 router.get('/', async (req, res, next) => {
     try {
-      const results = await db.query(`SELECT * FROM users`);
+      const results = await db.query(`SELECT * FROM companies`);
       return res.json({ companies: results.rows })
     } catch (e) {
       return next(e);
@@ -16,3 +16,19 @@ router.get('/', async (req, res, next) => {
   })
 
 module.exports = router
+
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const results = await db.query ('SELECT * FROM companies WHERE id =$1', [id])
+    if (results.rows.length === 0) {
+      throw new ExpressError(`Company with id of ${id} does not exist.`, 404)
+    }
+    return res.send({ company: results.rows[0] })
+  } catch (e) {
+    return next (e)
+  }
+})
+
+
