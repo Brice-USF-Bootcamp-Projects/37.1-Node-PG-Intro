5b0ebba26c60994e328080ba51d28a3d24359175
Brice Nelson <bnelsonemail@icloud.com>
2025-01-08 20:55:01 -0500

    added get all invoices query to the invoices route.

--- routes/invoices.js
+++ routes/invoices.js
@@ -1,11 +1,18 @@
 // routes/invoices.js
 
 const express = require("express");
+const ExpressError = require('../expressError')
 const router = express.Router();
+const db = require('../db')
 
-// Example route
-router.get("/", (req, res, next) => {
-  res.send("Invoices route");
+// Return info on invoices: like {invoices: [{id, comp_code}, ...]}
+router.get("/", async (req, res, next) => {
+  try {
+    const results = await db.query('SELECT * FROM invoices');
+    return res.json({ invoices: results.rows })
+  } catch (e) {
+    return next (e)
+  }
 });
 
 module.exports = router;
